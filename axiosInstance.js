5168b37932bed6fc19ac6ba102d0ff4b5c6e5fc3
sopascampeao <fabiomanuel@msn.com>
2022-01-27 17:41:06 -0100

    teste

---
+++ axiosInstance.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://127.0.0.1:80/',
+    timeout: 5000,
+    headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+
+
+    export default axiosInstance;
